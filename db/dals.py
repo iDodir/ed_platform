c734dd053dc6a1859b7234ad828036d3d88a41c1
dodir <mokronosovmw@gmail.com>
2023-10-28 12:44:11 +0500

    lesson 3: new endpoints for user, new tests

--- db/dals.py
+++ db/dals.py
@@ -1,3 +1,7 @@
+from typing import Optional
+from uuid import UUID
+
+from sqlalchemy import update, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import User
@@ -16,3 +20,32 @@ class UserDAL:
         self.db_session.add(new_user)
         await self.db_session.flush()
         return new_user
+
+    async def delete_user(self, user_id: UUID) -> Optional[UUID]:
+        query = (
+            update(User)
+            .where(User.user_id == user_id, User.is_active == True)
+            .values(is_active=False)
+            .returning(User.user_id)
+        )
+        res = await self.db_session.execute(query)
+        await self.db_session.flush()
+        return res.scalar()
+
+    async def get_user_by_id(self, user_id: UUID) -> Optional[User]:
+        query = select(User).where(User.user_id == user_id)
+        res = await self.db_session.execute(query)
+        await self.db_session.flush()
+        return res.scalar()
+
+    async def update_user(self, user_id: UUID, **kwargs) -> Optional[UUID]:
+        query = (
+            update(User)
+            # .where(User.user_id == user_id, User.is_active == True)
+            .where(User.user_id == user_id)
+            .values(**kwargs)
+            .returning(User.user_id)
+        )
+        res = await self.db_session.execute(query)
+        await self.db_session.flush()
+        return res.scalar()
--- db/session.py
+++ db/session.py
@@ -5,7 +5,12 @@ from sqlalchemy.orm import sessionmaker
 
 import settings
 
-engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
+engine = create_async_engine(
+    settings.REAL_DATABASE_URL,
+    future=True,
+    echo=True,
+    # execution_options={"isolation_level": "AUTOCOMMIT"},
+)
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
