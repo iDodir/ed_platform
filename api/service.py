418e3ed2b1cefe1380cf1ea7268e21ea32f0fb48
dodir <mokronosovmw@gmail.com>
2023-11-02 15:29:32 +0500

    lesson 10: add sentry

--- api/service.py
+++ api/service.py
@@ -5,9 +5,4 @@ service_router = login_router = APIRouter()
 
 @service_router.get("/ping")
 async def ping():
-    # import random
-    # err = random.choice(
-    #     [ZeroDivisionError, ValueError, TypeError],
-    # )
-    # raise err
     return {"Success": True}
